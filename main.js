9e2c14513e763664fdb292029ce874d6c40536b6
ollaariane <145364174+ollaariane@users.noreply.github.com>
2025-06-12 08:01:39 -0300

    ola

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-onst numeroSenha = document.querySelector('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
